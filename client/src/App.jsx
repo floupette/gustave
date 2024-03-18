10eb06ec80737a4a84b6c7e31f6523508f8a4ee3
malikamezaoui <84177863+malikamezaoui@users.noreply.github.com>
2024-03-18 09:52:14 +0100

    version finale front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,46 +7,48 @@ import { Route, Routes, useNavigate } from "react-router-dom"
 import Resultat from "./pages/Resultat/Resultat.jsx"
 import Detail from "./pages/Detail/Detail.jsx"
 import Profil from "./pages/Profil/Profil.jsx"
+import Footer from "./components/Footer/Footer.jsx"
 
 
 const App = () => {
-    // State pour gérer la data du fetch sur l'Utilisateur
-    const [user, setUser] = useState()
+  // State pour gérer la data du fetch sur l'Utilisateur
+  const [user, setUser] = useState()
 
-    // UseNavigate pour renvoyer l'utilisateur vers login s'il n'est pas connecté
-    const navigation = useNavigate()
+  // UseNavigate pour renvoyer l'utilisateur vers login s'il n'est pas connecté
+  const navigation = useNavigate()
 
-    /*  Fetch pour obtenir toutes les infos de l'utilisateur
-        Credentials permet d'envoyer le cookie SID (identification de session), 
-        ça évite un conflit avec le Back pour les CORS
-    */
-    useEffect(() =>{
-        fetch(`http://localhost:3630/auth/info`, {credentials: 'include'})
-            .then(Resultat => Resultat.json())
-            .then(Data => {
-                setUser(Data);
-            })
-            .catch(Error => {
-                /* Permet de gérer l'erreur.
-                  S'il y a une erreur d'authentification alors on redirige vers Login
-                */
-                navigation('/connexion'); 
-            })
-    },[])
+  /*  Fetch pour obtenir toutes les infos de l'utilisateur
+      Credentials permet d'envoyer le cookie SID (identification de session), 
+      ça évite un conflit avec le Back pour les CORS
+  */
+  useEffect(() => {
+    fetch(`http://localhost:3630/auth/info`, { credentials: 'include' })
+      .then(Resultat => Resultat.json())
+      .then(Data => {
+        setUser(Data);
+      })
+      .catch(Error => {
+        /* Permet de gérer l'erreur.
+          S'il y a une erreur d'authentification alors on redirige vers Login
+        */
+        navigation('/connexion');
+      })
+  }, [])
 
-    return (
-      <UserContext.Provider value={{user, setUser}}>
-          <Navbar />
-            <Routes>
-              <Route path='/connexion' element={<Connexion/>} />
-              <Route path='/nous' element={<h1>Nous</h1>} />
-              <Route path='/' element={<Home/>} />
-              <Route path='/resultat' element={<Resultat/>} />
-              <Route path='/detaillogement' element={<Detail/>} />
-              <Route path='/profil' element={<Profil/>}/>
-            </Routes>
-      </UserContext.Provider>
-    )
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Navbar />
+      <Routes>
+        <Route path='/connexion' element={<Connexion />} />
+        <Route path='/nous' element={<h1>Nous</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/resultat' element={<Resultat />} />
+        <Route path='/detaillogement' element={<Detail />} />
+        <Route path='/profil' element={<Profil />} />
+      </Routes>
+      <Footer/>
+    </UserContext.Provider>
+  )
 }
 
 export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,28 +1,59 @@
+import { useState, useEffect } from 'react';
+import Footer from "../../components/Footer/Footer";
 import SearchBar from "../../components/Searchbar/SearchBar";
-import "./home.css"
-
+import { Link } from 'react-router-dom';
+import "./home.css";
 
 const Home = () => {
+    const [logements, setLogements] = useState([]);
+
+    // Fonction pour mélanger et prendre trois logements de manière aléatoire
+    const getRandomLogements = (logements) => {
+        return logements.sort(() => 0.5 - Math.random()).slice(0, 3);
+    };
+
+    useEffect(() => {
+        fetch('http://localhost:3630/logements', {credentials: 'include'})
+            .then(response => response.json())
+            .then(data => {
+                setLogements(data);
+            })
+            .catch(error => {
+                console.log('Erreur lors de la récupération des logements:', error);
+                // Gère l'erreur comme nécessaire
+            });
+    }, []);
+
+    // Choisis aléatoirement trois logements une fois les logements chargés
+    const randomLogements = getRandomLogements(logements);
 
     return (
         <>
             <div className="image-presentation">
                 <div className="blur-background">
                     <div className="text-overlay">
-                        <p> “Explorez au-delà des frontières,</p>
+                        <p>“Explorez au-delà des frontières,</p>
                         <p>là où chaque instant sculpte un souvenir,</p>
                         <p>et chaque lieu révèle une nouvelle perspective sur la vie.”</p>
                     </div>
                 </div>
-
             </div>
             <div className="Search-bar">
                 <SearchBar />
             </div>
-            <div className="slider">
-                
+            <div className="coups-de-coeur">
+                {randomLogements.map((logement, index) => (
+                    logement.images.map((image, idx) => (
+                        idx === 0 && (
+                            <Link to={`/detaillogement`} state={{detaillogement : logement}} key={logement}>
+                                <img src={image} alt={`Coup de coeur ${index}`} className="coup-de-coeur-image" />
+                            </Link>
+                        )
+                    ))
+                ))}
             </div>
         </>
     );
 };
+
 export default Home;
